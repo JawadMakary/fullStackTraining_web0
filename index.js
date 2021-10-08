0755f3e2e0ca2c7f2b9e8432b1fdef9190a85705
jawad <jawadhalbymakary@gmail.com>
2021-10-08 13:30:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const xlsx = require("xlsx");
+const express = require("express");
+const app = express();
+var cors = require("cors");
+var bodyParser = require("body-parser");
+const { write } = require("fs");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.json());
+app.use(cors()); // middle ware : CORS : Cross Origin Resource Sharing
+// GET ALL DATA
+var wBook = xlsx.readFile("users.xlsx");
+var wSheet = wBook.Sheets[wBook.SheetNames[0]];
+var excelData = xlsx.utils.sheet_to_json(wSheet);
+app.get("/", function (req, res) {
+  res.send(excelData);
+  console.log(excelData.length);
+});
+// add user
+app.post("/addUser", function (req, res) {});
+
+// GET SPECIFIC USER (QUERY STRING)
+app.get("/user", function (req, res) {
+  let psw;
+  for (let i = 0; i < excelData.length; i++) {
+    const email = excelData[i].email;
+
+    if (req.query.email == email) {
+      // console.log(excelData[i].password);
+      psw = excelData[i].password;
+    } else {
+      // console.log("no query");
+    }
+  }
+  // console.log(psw);
+  if (psw) {
+    res.send(psw.toString());
+  } else {
+    res.send("no account was found");
+  }
+});
+
+app.listen(3030);
---
+++ question.md
@@ -0,0 +1,16 @@
+# Your Exercise is the following:
+
+ # *. Create API (NodeJS Express) having the following endPoints:
+#    *. Users   (Returns all Users from Excel File) 
+  #    Hint:It's GET Method that simply reads the Excel File
+   #        User Information : email + Password
+ 
+  # *. GetUser (Returns a specific user having its email)
+   #   Hint:It's GET Method that accepts a query string
+
+   # *. AddUser (Adds User to Excel file)
+   #  Hint:It's POST Method where the BODY contains the "email" & "password"
+
+   # *. Authenticate (Check if the email & password are valid)
+   # Hint:It's POST Method where the BODY contains the "email" & "password"
+   #        and returns true or false.
