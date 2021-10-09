8c8248c54443055c9928df8b469e7093f39f549f
jawad <jawadhalbymakary@gmail.com>
2021-10-09 16:26:21 +0300

    workin on addin user

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const xlsx = require("xlsx");
 const express = require("express");
 const app = express();
 var cors = require("cors");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const { write } = require("fs");
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json());
 app.use(cors()); // middle ware : CORS : Cross Origin Resource Sharing
 // GET ALL DATA
@@ -13,16 +12,26 @@ var wSheet = wBook.Sheets[wBook.SheetNames[0]];
 var excelData = xlsx.utils.sheet_to_json(wSheet);
 app.get("/", function (req, res) {
   res.send(excelData);
-  console.log(excelData.length);
+  // console.log(excelData.length);
 });
 // add user
-app.post("/addUser", function (req, res) {
-  let data = req.body;
-  data += excelData;
+// app.post("/addUser", function (req, res) {
+//   let data = { email: "123", password: 132 };
+//   data = { ...excelData } + data;
+//   const ws = xlsx.utils.json_to_sheet(excelData);
+//   const wb = xlsx.utils.book_new();
+//   xlsx.utils.book_append_sheet(wb, ws, "Responses");
+//   xlsx.writeFile(wb, "sampleData.export.xlsx");
+//   res.send(data);
+// });
 
-  res.send("user added");
+app.post("/addNewUser", function (req, res) {
+  console.log(req.body);
+  excelData.push(req.body);
+  // excelpush = xlsx.utils.json_to_sheet(excelData);
+  res.send(excelData);
+  wBook.Sheets(wSheet).push(req.body);
 });
-
 // GET SPECIFIC USER (QUERY STRING)
 app.get("/user", function (req, res) {
   let psw;
@@ -44,4 +53,6 @@ app.get("/user", function (req, res) {
   }
 });
 
-app.listen(3030);
+app.listen(3030, () => {
+  console.log("the server is running");
+});
